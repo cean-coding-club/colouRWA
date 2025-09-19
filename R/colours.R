e33aab2002b44a9fd24a93c4477d6889f0ed829a
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-09-19 14:35:55 +0800

    add Lake Hillier colours

---
+++ R/colours.R
@@ -0,0 +1,19 @@
+#' A Collection of Western Australia Colour Palettes
+#'
+#' A collection of colour palettes inspired by the landscape and wildlife of
+#'  Western Australia. The list of available palettes is:
+#' \describe{
+#'      \item{Lake Hillier}{Diverging palette based on the colours of Lake Hillier}.
+#' }
+#' @examplesIf interactive()
+#'
+#' scales::show_col(wa_pal[[1]])
+#'
+#' @source
+#' \describe{
+#'  \item{Lake Hillier}{<https://upload.wikimedia.org/wikipedia/commons/f/fb/Lake_Hillier_Shoreline_Pink_Hue_Salt_Deposite.jpg>}
+#' }
+#' @export
+wa_pal <- list(
+  "Lake Hillier" = c("#794954", "#b5635e", "#d2c6c9", "#325684", "#28211b")
+)
